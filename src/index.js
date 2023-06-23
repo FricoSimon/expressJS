b209d344de87db7a7f13e05547c7491907ba6cdb
FricoSimon <simonfriko@gmail.com>
2023-06-23 22:20:42 +0700

    test response send

--- src/index.js
+++ src/index.js
@@ -4,13 +4,31 @@ const dotenv = require('dotenv');
 dotenv.config();
 const port = process.env.PORT;
 const apiRouter = express.Router();
+const { responseGet, responsePost, responseError } = require('./response');
 
-apiRouter.get('/*', (req, res) => {
-    res.status(404).send('Not found!');
+apiRouter.get('/user', (req, res) => {
+    responseGet(200, 'Success!', 1, 1, {
+        name: 'John Doe',
+        age: 20,
+    }, res);
 });
 
+apiRouter.post('/user', (req, res) => {
+    responsePost(200, 'Success!', {
+        name: 'John Doe',
+        age: 20,
+    }, res);
+});
+
+// if route not found
+apiRouter.get('/*', (req, res) => {
+    responseError(404, 'Not found!', 'Route not found!', res);
+});
+
+// router /api/* will be handled by apiRouter
 app.use('/api', apiRouter);
 
+// error handler
 apiRouter.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(404).send('Error Occured!');
