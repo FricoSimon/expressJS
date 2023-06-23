e3773575e3fde31ee4b27b4b0c3622c2661aee44
FricoSimon <simonfriko@gmail.com>
2023-06-23 21:57:16 +0700

    add error handling and router /api

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const dotenv = require('dotenv');
+dotenv.config();
+const port = process.env.PORT;
+const apiRouter = express.Router();
+
+apiRouter.get('/*', (req, res) => {
+    res.status(404).send('Not found!');
+});
+
+app.use('/api', apiRouter);
+
+apiRouter.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(404).send('Error Occured!');
+})
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
