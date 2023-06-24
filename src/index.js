581170465245ff0a39bd18ef792016bee6129ce8
FricoSimon <simonfriko@gmail.com>
2023-06-24 20:22:20 +0700

    remove user route from main

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
 dotenv.config();
+
 const port = process.env.PORT;
 const apiRouter = express.Router();
+const userRoutes = require('../routes/user');
 const { responseGet, responsePost, responseError } = require('./response');
 
 // use json and urlencoded middleware
@@ -19,49 +21,19 @@ const logging = (req, res, next) => {
 // use logging middleware
 app.use(logging);
 
-apiRouter.get('/user', (req, res) => {
-    responseGet(200, 'Success!', 1, 1, [{
-        id: 1,
-        name: 'John Doe',
-        age: 20,
-    },
-    {
-        id: 2,
-        name: 'Jane Doe',
-        age: 22,
-    }
-    ], res);
-});
-
-apiRouter.get('/user/:id', (req, res) => {
-    responseGet(200, 'Success!', 1, 1, {
-        id: 1,
-        name: 'John Doe',
-        age: 20,
-    }, res);
-});
-
-apiRouter.post('/user', (req, res) => {
-    responsePost(200, 'Success!', {
-        id: 1,
-        name: 'John Doe',
-        age: 20,
-    }, res);
-});
-
 // if route not found
 apiRouter.get('/*', (req, res) => {
     responseError(404, 'Not found!', 'Route not found!', res);
 });
-
-// router /api/* will be handled by apiRouter
-app.use('/api', apiRouter);
 
 // error handler
 apiRouter.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(404).send('Error Occured!');
 })
+
+app.use('/api/user', userRoutes);
+app.use('/api', apiRouter);
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
