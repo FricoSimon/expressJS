228a87af3c54847d3eaff3565ddb8d1a62922dc7
FricoSimon <simonfriko@gmail.com>
2023-06-24 20:20:15 +0700

    create new user routes file

---
+++ routes/user.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const userRoutes = express.Router();
+const { responseGet, responsePost, responseError } = require('../src/response');
+
+// get all users
+userRoutes.get('', (req, res) => {
+    responseGet(200, 'Success!', 1, 1, [{
+        id: 1,
+        name: 'John Doe',
+        age: 20,
+    },
+    {
+        id: 2,
+        name: 'Jane Doe',
+        age: 22,
+    }
+    ], res);
+});
+
+// get user by id
+userRoutes.get('/:id', (req, res) => {
+    responseGet(200, 'Success!', 1, 1, {
+        id: 1,
+        name: 'John Doe',
+        age: 20,
+    }, res);
+});
+
+// create new user
+userRoutes.post('', (req, res) => {
+    responsePost(200, 'Success!', {
+        id: 1,
+        name: 'John Doe',
+        age: 20,
+    }, res);
+});
+
+// if route not found
+userRoutes.get('/*', (req, res) => {
+    responseError(404, 'Not found!', 'Route not found!', res);
+});
+
+// error handler
+userRoutes.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(404).send('Error Occured!');
+})
+
+module.exports = userRoutes;
