66fe88201ce71caae1260d143ba526650ffbaa92
FricoSimon <simonfriko@gmail.com>
2023-06-25 14:17:02 +0700

    add dummy login route with session

---
+++ routes/auth.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const authRoutes = express.Router();
+const { responseGet, responsePost, responseError } = require('../src/response');
+
+authRoutes.post('/login', (req, res) => {
+    const { username, password } = req.body;
+    if (username && password) {
+        if (req.session.username) {
+            responsePost(200, 'Successful!', { username, password }, res);
+        } else {
+            req.session.username = username;
+            responsePost(200, 'Success!', { username, password }, res);
+        }
+    } else {
+        responseError(400, 'Bad Request!', 'Username and password are required!', res);
+    }
+});
+
+module.exports = authRoutes;
