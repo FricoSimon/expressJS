f937e974f2c5f8b95c30fcab88fffd8332ef2de5
FricoSimon <simonfriko@gmail.com>
2023-06-28 09:46:45 +0700

    create login with mongodb

--- routes/auth.js
+++ routes/auth.js
@@ -4,19 +4,34 @@ const { responseGet, responsePost, responseError } = require('../src/response');
 const User = require('../database/schema/user');
 const { hashPassword, comparePassword } = require('../utils/hash');
 
-authRoutes.post('/login', (req, res) => {
-    const { username, password } = req.body;
-
+// login route
+authRoutes.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+    if (!email || !password) responseError(400, 'Bad Request!', 'Username or password is required!', res);
+    if (email && password) {
+        const userDB = await User.findOne({ email: email });
+        if (!userDB) {
+            responseError(400, 'Bad Request!', 'Username is incorrect!', res);
+        } else {
+            const isMatch = await comparePassword(password, userDB.password);
+            if (isMatch) {
+                responsePost(200, 'Login Successful!', userDB, res);
+            } else {
+                responseError(400, 'Bad Request!', 'Password is incorrect!', res);
+            }
+        }
+    }
 });
 
+// register route
 authRoutes.post('/register', async (req, res) => {
-    const { username, email } = req.body;
+    const email = req.body.email;
     const userDB = await User.findOne({ email });
     if (userDB) {
         responseError(400, 'Bad Request!', 'Username or email already exists!', res);
     } else {
         const password = await hashPassword(req.body.password);
-        const newUser = await User.create({ username, password, email });
+        const newUser = await User.create({ email, password });
         responsePost(200, 'Success!', newUser, res);
     }
 });
